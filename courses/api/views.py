6c22dffaf36a973c1130947cb2c06181e6f69f1a
smarak1996 <smarakprd5@gmail.com>
2022-04-01 17:51:53 +0530

    Django Rest Framework

--- courses/api/views.py
+++ courses/api/views.py
@@ -9,7 +9,8 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .serializers import CourseSerializer
-from rest_framework.decorators import detail_route
+#from rest_framework.decorators import detail_route
+from rest_framework.decorators import action
 from .permissions import IsEnrolled
 from .serializers import CourseWithContentsSerializer
 
@@ -34,12 +35,14 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-    @detail_route(methods=['post'], authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated])
+    #@detail_route(methods=['post'], authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated])
+    @action(detail=True,methods=['post'],authentication_classes=[BasicAuthentication],permission_classes=[IsAuthenticated])
     def enroll(self, request, *args, **kwargs):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
 
-    @detail_route(methods=['get'], serializer_class=CourseWithContentsSerializer, authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated, IsEnrolled])
+    #@detail_route(methods=['get'], serializer_class=CourseWithContentsSerializer, authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated, IsEnrolled])
+    @action(detail=True,methods=['post'],authentication_classes=[BasicAuthentication],permission_classes=[IsAuthenticated])
     def contents(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
